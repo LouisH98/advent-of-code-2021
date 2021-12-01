f0782e90fd376ef794280afbde62dfd081a28a6b
Louis Holdsworth <louisholdsworth@gmail.com>
2021-12-01 14:04:28 +0000

    Added second solution to day 1

--- 1-sonar-sweep/solution.js
+++ 1-sonar-sweep/solution.js
@@ -1,19 +1,41 @@
 const fs = require('fs')
 let measurements = fs.readFileSync('./measurements.txt').toString().split('\n').map(m => Number(m));
 
-const countOfIncreased = measurements.reduce((accumulator, currentValue, currentIndex, array) => {
-  let lastValue;
-
-  try{
-    lastValue = array[currentIndex - 1];
-  } finally {
-    if(lastValue && currentValue > lastValue) {
-      return ++accumulator;
-    } else {
-      return accumulator;
-    }
-  }
+function part1(input){
+  return input.reduce((accumulator, currentValue, currentIndex, array) => {
+    let lastValue;
   
-}, 0)
+    try{
+      lastValue = array[currentIndex - 1];
+    } finally {
+      if(lastValue && currentValue > lastValue) {
+        return ++accumulator;
+      } else {
+        return accumulator;
+      }
+    }
+    
+  }, 0)
+}
 
-console.log(`Result is: ${countOfIncreased}`);
+function part2(input) {
+  let lastSum = -1;
+  let count = 0;
+  for (let index = 0; index < input.length; index++) {
+    const [one, two, three] = input.slice(index);
+
+    if(!(one || two || three)) break;
+
+    const sum = one + two + three;
+    if(sum > lastSum && lastSum !== -1) {
+      count++;
+    }
+
+    lastSum = sum;
+  }
+  return count;
+}
+
+
+console.log(`Part 1: ${part1(measurements)}`);
+console.log(`Part 2: ${part2(measurements)}`);
