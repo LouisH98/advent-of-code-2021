3d0429d03416fee3a9a89b9d766ac835a23a05b0
Louis Holdsworth <louisholdsworth@gmail.com>
2021-12-02 09:26:49 +0000

    Change solution into a function for part 1

--- 2-dive/solution.js
+++ 2-dive/solution.js
@@ -1,32 +1,37 @@
 const fs = require('fs')
 let measurements = fs.readFileSync('./measurements.txt').toString().split('\n');
 
-const subPosition = {
-    horizontal: 0,
-    depth: 0
-}
-
-let mapMeasurement = (direction, amount) => {
-    let directionMap =  {
-        up: 'depth',
-        down: 'depth',
-        forward: 'horizontal',
+const part1 = () => {
+    const subPosition = {
+        horizontal: 0,
+        depth: 0
+    }
+    
+    let mapMeasurement = (direction, amount) => {
+        let directionMap =  {
+            up: 'depth',
+            down: 'depth',
+            forward: 'horizontal',
+        }
+    
+        if(direction === 'up') {
+            amount = -amount
+        }
+    
+        return [directionMap[direction], amount]
+      
+    }
+    
+    for(const measurement of measurements) {
+        let [direction, amount] = measurement.replace("\r", "").split(" ");
+        amount = Number(amount);
+    
+        [direction, amount] = mapMeasurement(direction, amount)
+        subPosition[direction] += amount
     }
 
-    if(direction === 'up') {
-        amount = -amount
-    }
-
-    return [directionMap[direction], amount]
-  
+    return subPosition['horizontal'] * subPosition['depth']
 }
 
-for(const measurement of measurements) {
-    let [direction, amount] = measurement.replace("\r", "").split(" ");
-    amount = Number(amount);
 
-    [direction, amount] = mapMeasurement(direction, amount)
-    subPosition[direction] += amount
-}
-
-console.log(subPosition['horizontal'] * subPosition['depth']);
+console.log(`Part 1: ${part1()}`);
