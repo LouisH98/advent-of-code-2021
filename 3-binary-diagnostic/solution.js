b70cdc90dfba511888d0ea5cfad82a01220aab76
Louis Holdsworth <louisholdsworth@gmail.com>
2021-12-04 17:24:07 +0000

    Upload code, incomplete

--- 3-binary-diagnostic/solution.js
+++ 3-binary-diagnostic/solution.js
@@ -4,8 +4,11 @@ let measurements = fs.readFileSync('./measurements.txt')
                         .replace(/\r/g, "")
                         .split('\n')
 
-//gets most often occuring element in array
-function mode(array) {
+const LENGTH_OF_MEASUREMENT = 12;
+
+// gets most often occuring element in array
+// if shouldDraw: returns '-' if there is a equal number of 1s and 0s
+function mode(array, shouldDraw) {
     const elemMap = {};
     array.forEach(bit => {
         if(!(bit in elemMap)) { 
@@ -15,7 +18,8 @@ function mode(array) {
         elemMap[bit]++;
     })
 
-    return Object.keys(elemMap).reduce(function(a, b){ return elemMap[a] > elemMap[b] ? a : b });
+    if(shouldDraw && elemMap['0'] === elemMap['1']) return '-';
+    return  Object.keys(elemMap).reduce(function(a, b){ return elemMap[a] > elemMap[b] ? a : b });
 }
 
 function notBinString(binString) {
@@ -23,7 +27,6 @@ function notBinString(binString) {
 }
 
 function part1() {
-    const LENGTH_OF_MEASUREMENT = 12;
     
     let calculateGamma = () => {
         let gamma = ""; 
@@ -45,4 +48,30 @@ function part1() {
     return parseInt(gamma, 2) * parseInt(epsilon, 2); 
 }
 
+function part2() {
+
+    const getIndexesOfValue = (value, array) => {
+        return array.map((elem, index) => {
+            if(elem === value) return index;
+        }).filter(elem => !isNaN(elem))
+    }
+
+    const removeIndexesFromArray = (source, indexes) => {
+        return source.filter((_, index) => indexes.includes(index));
+    }
+
+    let getRating = () => {
+        let filteredMeasurements = [...measurements];
+        for (let index = 0; index < LENGTH_OF_MEASUREMENT; index++) {
+            const columnList = measurements.map(elem => elem[index]);
+            const mostOftenOccuring = mode(columnList);
+            const indexesToRemove = getIndexesOfValue(mostOftenOccuring === '1' ? '0' : '1', columnList);
+            filteredMeasurements = removeIndexesFromArray(filteredMeasurements, indexesToRemove);
+        }
+    }
+
+    getRating();
+}
+
 console.log(`Part 1: ${part1()}`);
+console.log(`Part 2: ${part2()}`);
