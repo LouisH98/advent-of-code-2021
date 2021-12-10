782745a637908cc6cec4e480e7a643ca82fc00b3
Louis Holdsworth <louisholdsworth@gmail.com>
2021-12-10 09:29:29 +0000

    working on day 9 part 2

--- 9-smoke-basin/solution.js
+++ 9-smoke-basin/solution.js
@@ -8,69 +8,22 @@ let inputFile = fs.readFileSync('./input.txt')
 
 function isLowPoint(i, j, array) {
     const value = array[i][j];
-    
-    let isLeftHigher;
-    let isAboveHigher;
-    let isRightHigher;
-    let isBelowHigher;
-    
-    leftValue = array[i][j-1];
-    if(leftValue !== undefined){
-        isLeftHigher = leftValue > value;
-    } else {
-        isLeftHigher = true;
-    }
 
-    try {
-        isAboveHigher = array[i - 1][j] > value;
-    } catch {
-        isAboveHigher = true;
-    }
-
-    rightValue = array[i][j + 1];
-    if(rightValue !== undefined) {
-        isRightHigher = rightValue > value;
-    } else {
-        isRightHigher = true;
-    }
-
-    try {
-        isBelowHigher = array[i + 1][j] > value;
-    } catch  {
-        isBelowHigher = true;
-    }
-
+    const isLeftHigher = array[i][j-1] === undefined ? true : array[i][j-1] > value;
+    const isAboveHigher = array[i - 1] === undefined ? true : array[i - 1][j] > value;
+    const isRightHigher = array[i][j+1] === undefined ? true : array[i][j+1] > value;
+    const isBelowHigher = array[i + 1] === undefined ? true : array[i + 1][j] > value;
 
     return isLeftHigher && isAboveHigher && isRightHigher && isBelowHigher;
 }
 
 
 function findBasin(i, j, array) {
-    let currentBasin = [];
-    let expanding = true;
+   let basinCoords = [];
 
-    let currentXTarget = i;
-    let currentYTarget = j;
-
-    while(expanding) {
-        for (let x = -currentXTarget; i <= currentXTarget; x++) {
-            for (let y = -currentYTarget; y <= currentYTarget; y++) {
-                let value;
-                try {
-                    value = array[x][y];
-                } catch {
-                    continue;
-                }
-            }        
-        }
-
-        currentXTarget++;
-        currentYTarget++;
-    }
    
-
-    return currentBasin;
 }
+
 
 function findBasins(array){
     let basins = [];
